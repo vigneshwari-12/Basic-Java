0477168f5ec2def2e236ee0476b47326c3ba0092
vigneshwari-12 <141740385+vigneshwari-12@users.noreply.github.com>
2024-11-13 21:51:26 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import Java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        System.out.println("Welcome to Java Programming");
+        System.out.println(name);
+        sc.close();
+    }
+}
